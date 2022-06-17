92a5ed62a7d9b046729faa4239178630568cea0a
qqqbbb <qqqinbox@ya.ru>
2022-06-17 12:39:17 +0300

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
-        System.out.println("Hello !");
+interface FirstInterface {
+    public static int qqq = 1;
+    public void myMethod(); // interface method
+}
+
+interface SecondInterface {
+    public void myOtherMethod(); // interface method
+}
+
+// DemoClass "implements" FirstInterface and SecondInterface
+class DemoClass implements FirstInterface, SecondInterface {
+    public void myMethod() {
+        System.out.println("Some text..");
+    }
+    public void myOtherMethod() {
+        System.out.println("Some other text...");
+    }
+}
+class DemoClass_ implements FirstInterface {
+    public void myMethod() {
+        System.out.println("Some text..");
+    }
+
+}
+class Main {
+    public static void main(String[] args) {
+        DemoClass myObj = new DemoClass();
+        char[] chars = new char[11];
+        System.out.println("chars[1] " + chars[1]);
+//        DemoClass.qqq = 6;
+        myObj.myMethod();
+        myObj.myOtherMethod();
     }
 }
