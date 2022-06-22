f99d88d6082257f1f0c702e7147de760078a1b40
qqqbbb <qqqinbox@ya.ru>
2022-06-22 11:40:58 +0300

    commit 4

---
+++ src/Test1.java
@@ -0,0 +1,11 @@
+public class Test1
+{
+    public static void main(String[] args) {
+//        DemoClass myObj = new DemoClass();
+//        char[] chars = new char[11];
+//        System.out.println("chars[1] " + chars[1]);
+////        DemoClass.qqq = 6;
+//        myObj.myMethod();
+//        myObj.myOtherMethod();
+    }
+}
